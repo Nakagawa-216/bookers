7fd09460c5b3ada2de1a39bf80024329a76d0326
KouNakagawa <rarai0626@gmail.com>
2021-07-09 08:07:44 +0000

    [Update] Validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
       flash[:success] = 'Book was successfully created.'
     else
+      # @book = Book.new
+      @books = Book.all
       render :index
     end
   end
@@ -26,10 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-    flash[:notice] = 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
